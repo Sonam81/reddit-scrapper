42ce85c6740510a3213d197ea180381038764fc3
Sonam Sherpa <sonam.sherpa1980@gmail.com>
2023-04-02 16:19:07 -0400

    Fixed issue with POST requests

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 # Import libraries and user defined functions
-from reddit_api_handler import get_post, show_post_lists
+from reddit_api_handler import get_posts
 from fastapi import FastAPI, Request, Body
 from fastapi.middleware.cors import CORSMiddleware
 from scrapper import list_popular
+import uvicorn
 
 # To run server
 # uvicorn main:app --reload
 
 # Build API Interface using FastAPI
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", host="127.0.0.1", port=8080)
+
 app = FastAPI()
+
 origins = [
     "*",
 ]
@@ -26,15 +31,14 @@ async def root():
 
 @app.get("/subreddit/{item_id}")
 async def get_request(item_id):
-    return get_post(item_id)
+    return get_posts(item_id)
 
 @app.post("/subreddit/", status_code=201)
-async def show_posts(count: int = Body(...), subreddit: str = Body(...), type: str = Body(...)):
-    return show_post_lists(count, subreddit, type)
+async def show_posts(post_limit: int = Body(...), subreddit: str = Body(...), type: str = Body(...)):
+    return get_posts(subreddit, type, post_limit)
 
 @app.get("/popular/{type}", status_code=201)
 async def show_popular(type):
-    print(type)
     scrap_url = 'https://www.reddit.com/r/popular/'
     scrap_url = scrap_url + type
     return list_popular(scrap_url)
--- reddit_api_handler.py
+++ reddit_api_handler.py
@@ -1,36 +1,28 @@
 from config import Scrapper
 
-# Local variables
-subreddit = 'MachineLearning'  # To be passed via. user interface
-post_limit = 10  # Static list count
-show_top_comments = False
-number_of_comments = 5
-
 scrapper = Scrapper()
 reddit = scrapper.initialize_praw()
 
-def get_post(subreddit):
+def get_posts(subreddit, type='top', post_limit=10):
     """
     Get posts under subbreddit using praw
     @param subreddit: Name of subreddit
     @return: Post details in JSON
     """
-    hot_posts = reddit.subreddit(subreddit).hot(limit=post_limit)
-    posts = []
-    for post in hot_posts:
+    return_posts = []
+
+    if type == 'hot':
+        all_posts = reddit.subreddit(subreddit).hot(limit=post_limit)
+    elif type == 'new':
+        all_posts = reddit.subreddit(subreddit).new(limit=post_limit)
+    else:
+        all_posts = reddit.subreddit(subreddit).top(limit=post_limit)
+
+    for post in all_posts:
         post_title = post.title
         post_id = post.id
         post_permalink = 'https://www.reddit.com' + post.permalink
         post_detail = {'id': post_id, 'title': post_title, 'permalink': post_permalink}
-        posts.append(post_detail)
-    return {'posts': posts}
+        return_posts.append(post_detail)
+    return {'posts': return_posts}
 
-
-def show_post_lists(count, subreddit, type):
-    """
-    Get top treding post using Beautifulsoap web scrapper
-    @param count: Count of the posts
-    @param subreddit:
-    @param type: Hot, New, Best etc
-    @return:
-    """
